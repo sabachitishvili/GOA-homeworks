54cae71fe028e09ec48cb16446e532405c75d1a7
sabachitishvili <chitishvilisaba4@gmail.com>
2024-02-15 18:04:54 +0400

    third commit

---
+++ day 2/homework/leasson2.py
@@ -0,0 +1,11 @@
+print("gavicani da davumegobrdi")
+
+#gugaberidze
+
+print("ras fiqrobt GOA-ze")
+
+#chemma megobarma gamacno goa da pirvelive gakvetili momewona rogorc xsnian, rogoric gamacno martlac egeti kargi yofila yvela dzalian megobrulia
+
+print("chaexutet dedikos/mamikos")
+
+#me dzalian madlieri var rom eseti kargi mshoblebi myavs da gverdshi midganan rac arunda iyos
