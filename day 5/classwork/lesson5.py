ade8adb1afa0f06ffe37bfdd047550eae713e959
sabachitishvili <chitishvilisaba4@gmail.com>
2024-02-19 00:16:22 +0400

    second commit

---
+++ day 5/classwork/lesson5.py
@@ -0,0 +1,5 @@
+num1 = int(input("number1"))
+num2 = int(input("number2"))
+num3 = int(input("number3"))
+
+print(num1 * num2 * num3)
---
+++ day 5/homework/lesson5.py
@@ -0,0 +1,33 @@
+#shemovitant input funqciit or ricxvs romelsac shevinaxavt num1shi da num2shi
+#amis shemdeg rac ki shedarebebi arsebobs matematikashi yvelafers gamovitant print-is sashualebit(+,-,*,/,<,>)
+
+#mimateba
+num1 = int(input("number1"))
+
+num2 = int(input("number2"))
+
+print (num1 + num2) 
+
+
+#gamokleba
+num1 = int(input("number1"))
+
+num2 = int(input("number2"))
+
+print (num1 - num2)
+
+
+#gamravleba
+num1 = int(input("number1"))
+
+num2 = int(input("number2"))
+
+print (num1 * num2) 
+
+
+#gayofa 
+num1 = int(input("number1"))
+
+num2 = int(input("number2"))
+
+print (num1 / num2) 
