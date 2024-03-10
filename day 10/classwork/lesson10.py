28288e4605fa1adcc911d650ac9b131aead1b6f3
sabachitishvili <chitishvilisaba4@gmail.com>
2024-03-10 17:48:32 +0400

    second commit

---
+++ day 10/classwork/lesson10.py
@@ -0,0 +1,19 @@
+# age = int(input("please enter your age"))
+
+# if age >= 0 and age < 13:
+#     print("ბავშვი")
+
+# elif age >= 13 and age < 18:
+#     print("თინეიჯერი")
+
+# elif age >= 18 and age < 60:
+#     print("სრულწლოვანი")
+
+# else:
+#     print("მოხუცი")
+
+
+movie_list = ["joker", "fightclub", "intersteral"]
+
+print (movie_list)
+print (movie_list[0])
