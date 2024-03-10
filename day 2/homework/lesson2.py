9592657b0ee15ab1c9f27ef8ed1a17da3c00fd1a
saba chitishvili <157018104+sabachitishvili@users.noreply.github.com>
2024-03-10 18:43:39 +0400

    Delete day 2/homework directory

--- day 2/homework/lesson2.py
+++
@@ -1,8 +0,0 @@
-print("gavicani da davumegobrdi")
-
-#gavicani nikoloz filishvili da guga beridze chemi razmis lideri
-
-print("ras vfiqrob GOA-ze")
-
-#pirvelad goaze chemma megobarma momiyva da rom shemovedi isetive situacia iyo rogoric mis monayolshi iyo, aq yvela dzalian megobrulia da yvea cdilobs rom dagexmaros aravin ardagcinebs tu rame arici mokled me dzalian momewona GOA.
- 
